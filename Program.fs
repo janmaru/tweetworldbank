60302d6f7a77e53bb0689e0861b8b9954e0b2315
janmaru <janmaru@gmail.com>
2017-02-22 14:52:00 +0100

    Aggiungere i file di progetto.

---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿//Altre informazioni su F# all'indirizzo http://fsharp.org
+// Per ulteriori informazioni, vedere il progetto 'Esercitazione su F#'.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // restituisci un intero come codice di uscita
